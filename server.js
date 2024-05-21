b72b80cde38d2de16d794bac49beedb55f570edf
Bhishman Desai <desaibhishman1@gmail.com>
2024-05-20 23:35:30 -0300

    feat: express router

--- server.js
+++ server.js
@@ -30,55 +30,19 @@ app.use(cors(corsOptions));
 // in other words, form data:
 // ‘content-type: application/x-www-form-urlencoded’
 app.use(express.urlencoded({extended: false}));
-
 // built-in middleware for json
 app.use(express.json());
-
 //serve static files
 app.use(express.static(path.join(__dirname, '/public')));
 
 
-app.get('^/$|/index(.html)?', (req, res) => {
-    //res.sendFile('./views/index.html', { root: __dirname });
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
-
-app.get('/new-page(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-});
-
-app.get('/old-page(.html)?', (req, res) => {
-    res.redirect(301, '/new-page.html'); //302 by default
-});
-
-// Route handlers
-app.get('/hello(.html)?', (req, res, next) => {
-    console.log('attempted to load hello.html');
-    next()
-}, (req, res) => {
-    res.send('Hello World!');
-});
+/* Routes */
+app.use('/', require('./routes/root'));
+app.use('/subdir', require('./routes/subdir'));
+app.use('/employees', require('./routes/api/employees'));
 
 
-// chaining route handlers
-const one = (req, res, next) => {
-    console.log('one');
-    next();
-}
-
-const two = (req, res, next) => {
-    console.log('two');
-    next();
-}
-
-const three = (req, res) => {
-    console.log('three');
-    res.send('Finished!');
-}
-
-app.get('/chain(.html)?', [one, two, three]);
-
-// 404
+/* 404 */
 app.all('*', (req, res) => {
     res.status(404);
     if (req.accepts('html')) {
@@ -93,6 +57,7 @@ app.all('*', (req, res) => {
 
 /* Error Handling Logic */
 app.use(errorHandler);
+
 
 /* App listen */
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
